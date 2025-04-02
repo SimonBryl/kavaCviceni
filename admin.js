cadb5691e24cb792f5e299ae116cd7c340649279
SimonBryl <s6mon07@gmail.com>
2025-04-02 14:28:45 +0200

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,4 +1,4 @@
-const firebaseURL = 'https://your-project-id.firebaseio.com'; // Replace with your Firebase URL
+const firebaseURL = 'https://kaves-5ee65-default-rtdb.europe-west1.firebasedatabase.app/'; // Replace with your Firebase URL
 
 const validUsername = "admin";
 const validPassword = "admin123";

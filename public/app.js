81986725168b179a5a0de4138ceef8ffc0f6eb46
SimonBryl <s6mon07@gmail.com>
2025-05-02 00:09:02 +0200

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,7 +1,7 @@
 let cart = [];
 
 async function loadProducts() {
-  const res = await fetch('/api/products');
+  const res = await fetch('/api/order');
   const products = await res.json();
   const container = document.getElementById('products');
   container.innerHTML = '';
